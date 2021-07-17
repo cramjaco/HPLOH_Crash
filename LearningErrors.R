e5e41a045d4f247a51360e07a327095055d777a4
Jacob A Cram <jcram@umces.edu>
2021-07-16 20:04:12 -0400

    Initial commit with DADA filtering issue

---
+++ LearningErrors.R
@@ -0,0 +1,22 @@
+library(tidyverse)
+library(dada2)
+packageVersion("dada2")
+
+files_to_filter <- read_csv("files_for_learning_errors.csv")
+
+nb = 1e8 # minimum number of bases
+mt = TRUE
+err_forward_all <- files_to_filter %>%
+  group_by(Run) %>%
+  nest() %>%
+  mutate(err_forward = map(data,
+                           ~learnErrors(.$FilteredPath_R1,
+                                        nbases = nb,
+                                        randomize = TRUE,
+                                        multithread = mt))) %>%
+  mutate(err_reverse = map(data, ~learnErrors(.$FilteredPath_R2,
+                                              nbases = nb,
+                                              randomize = TRUE,
+                                              multithread = mt)))
+
+save(files_to_filter, nb, err_forward_all, file = "LearningErrorsResults.RData")
---
+++ PullFastqFromSRA.sh
@@ -0,0 +1,1 @@
+cat SRR_Acc_List.txt | xargs fastq-dump --split-files --gzip --outdir SRA_Readfiles {}
